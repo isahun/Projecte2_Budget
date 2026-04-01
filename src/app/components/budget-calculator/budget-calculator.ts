c8cda4fb0d9e272d82bb2fd14d6fec4a0fc89c66
Irene Virgínia <isahun7@gmail.com>
2026-04-01 17:03:27 +0200

    feat: conditionally display web-customizer using angular control flow

--- src/app/components/budget-calculator/budget-calculator.ts
+++ src/app/components/budget-calculator/budget-calculator.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ServiceCard } from "../service-card/service-card";
 import { BudgetService } from '../../services/budget-service';
+import { WebCustomizer } from '../web-customizer/web-customizer';
 
 @Component({
   selector: 'app-budget-calculator',
-  imports: [ServiceCard],
+  imports: [ServiceCard, WebCustomizer],
   templateUrl: './budget-calculator.html',
   styleUrl: './budget-calculator.css',
 })
