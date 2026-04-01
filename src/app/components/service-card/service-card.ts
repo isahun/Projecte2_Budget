5c7267e72ae7958ce2a6a98a99a89b3c2e655952
Irene Virgínia <isahun7@gmail.com>
2026-04-01 16:33:12 +0200

    feat: create reusable service-card component with inputs

--- src/app/components/service-card/service-card.ts
+++ src/app/components/service-card/service-card.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Service } from '../../interfaces/budget-service.interface';
+import { BudgetService } from '../../services/budget';
 
 @Component({
   selector: 'app-service-card',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './service-card.html',
   styleUrl: './service-card.css',
 })
-export class ServiceCard {}
+export class ServiceCard {
+  // El "pare" ens passarà la informació del servei
+  @Input({ required: true }) service!: Service;
+
+  //Injectem el recepcionista, el servei
+  private budgetService = inject(BudgetService);
+
+  //Mètode per avisar que hem clicat el checkbox
+  toggleService() {
+    // Aquí cridarem a una funció del servei per avisar del canvi, la programarem al servei
+  }
+}
