3fa119fa0e6d68a007324d4968c7e831bb91834f
Irene Virgínia <isahun7@gmail.com>
2026-04-01 17:22:18 +0200

    feat: implement reactive budget-form with validation and error handling

--- src/app/components/budget-form/budget-form.ts
+++ src/app/components/budget-form/budget-form.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BudgetService } from '../../services/budget-service';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { validators } from '../../core/data/validators';
 
 @Component({
   selector: 'app-budget-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './budget-form.html',
   styleUrl: './budget-form.css',
 })
-export class BudgetForm {}
+export class BudgetForm {
+  private fb = inject(FormBuilder);
+  public budgetService = inject(BudgetService);
+
+  //Definim formulari amb validacions
+  budgetForm = this.fb.group(validators);
+
+  submitBudget() {
+    if (this.budgetForm.valid) {
+      //si form vàlid, creem objecte pressupost
+      const newBudget = {
+        ...this.budgetForm.value,
+        total: this.budgetService.totalPrice(),
+        date: new Date()
+      };
+
+      console.log('Pressupost llest per guardar:', newBudget);
+
+      this.budgetForm.reset(); //netegem form dp d'enviar
+    }
+  }
+
+
+}
