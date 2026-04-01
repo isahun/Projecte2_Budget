88e76b9faf255fdb57bd1fd3a92e5ad08542111b
Irene Virgínia <isahun7@gmail.com>
2026-04-01 18:02:20 +0200

    feat: create BudgetList with @Input to render budget collection

--- src/app/components/budget-list/budget-list.ts
+++ src/app/components/budget-list/budget-list.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BudgetCard } from '../budget-card/budget-card';
+import { Budget } from '../../interfaces/budget-service.interface';
 
 @Component({
   selector: 'app-budget-list',
-  imports: [],
+  imports: [BudgetCard],
   templateUrl: './budget-list.html',
   styleUrl: './budget-list.css',
 })
-export class BudgetList {}
+export class BudgetList {
+
+  @Input({ required: true }) budgets: Budget[] = [];
+}
