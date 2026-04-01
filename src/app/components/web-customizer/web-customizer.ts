28d89734230974d748013d9b200fcb41a450a99d
Irene Virgínia <isahun7@gmail.com>
2026-04-01 17:03:19 +0200

    feat: implement web-customizer component with step controls and styles

--- src/app/components/web-customizer/web-customizer.ts
+++ src/app/components/web-customizer/web-customizer.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BudgetService } from '../../services/budget-service';
 
 @Component({
   selector: 'app-web-customizer',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './web-customizer.html',
   styleUrl: './web-customizer.css',
 })
-export class WebCustomizer {}
+export class WebCustomizer {
+  public budgetService = inject(BudgetService);
+
+  updatePages(value: number) {
+    const current = this.budgetService.numPages();
+    //Validem, si intentem restar i ja som a 1, no fem res
+    if (value === -1 && current <= 1) return;
+    this.budgetService.numPages.set(current + value);
+  }
+
+  updateLanguages(value: number) {
+    const current = this.budgetService.numLanguages();
+    //Validem again
+    if (value === -1 && current <= 1) return;
+    this.budgetService.numLanguages.set(current + value);
+  }
+}
