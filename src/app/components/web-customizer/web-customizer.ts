6d062470fedfde5f54d4228725d582bfc2a4a9a3
Irene Virgínia <isahun7@gmail.com>
2026-04-02 09:28:02 +0200

    feat: integrate info modals into web customizer with signals

--- src/app/components/web-customizer/web-customizer.ts
+++ src/app/components/web-customizer/web-customizer.ts
@@ -1,14 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { BudgetService } from '../../services/budget-service';
+import { InfoModal } from '../info-modal/info-modal';
+import { INFO_MESSAGES } from '../../core/data/info-text';
 
 @Component({
   selector: 'app-web-customizer',
-  imports: [],
+  imports: [InfoModal],
   templateUrl: './web-customizer.html',
   styleUrl: './web-customizer.css',
 })
 export class WebCustomizer {
   public budgetService = inject(BudgetService);
+
+  isModalOpen = signal(false);
+  modalText = signal('');
 
   updatePages(value: number) {
     const current = this.budgetService.numPages();
@@ -22,5 +27,17 @@ export class WebCustomizer {
     //Validem again
     if (value === -1 && current <= 1) return;
     this.budgetService.numLanguages.set(current + value);
+  }
+
+  openInfo(type: 'pages' | 'languages') {
+    const pagesMsg = INFO_MESSAGES.pages;
+    const langMsg = INFO_MESSAGES.languages;
+
+    if (type === 'pages') {
+      this.modalText.set(pagesMsg);
+    } else {
+      this.modalText.set(langMsg);
+    }
+    this.isModalOpen.set(true);
   }
 }
