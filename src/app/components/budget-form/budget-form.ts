a54ab79492c563fec5082698a3b35a071697e63e
Irene Virgínia <isahun7@gmail.com>
2026-04-11 12:39:25 +0200

    refactor(budget-form): remove redundant inline comments

--- src/app/components/budget-form/budget-form.ts
+++ src/app/components/budget-form/budget-form.ts
@@ -32,36 +32,31 @@ export class BudgetForm {
           return s;
         });
 
-      //si form vàlid, creem objecte pressupost
       const newBudget = {
         id: Date.now(),
         clientName: this.budgetForm.value.name as string,
         clientEmail: this.budgetForm.value.email as string,
         clientPhone: this.budgetForm.value.phone as string,
-        //filtrem només serveis seleccionats en el moment
         services: selectedServices,
         total: this.budgetService.totalPrice(),
         date: new Date(),
       };
 
       try {
-        // 2. Esperem que el servei ens confirmi que s'ha guardat
         await this.budgetService.addBudget(newBudget);
 
 
-        // 3. Només si ha anat bé, netegem el formulari
       this.budgetForm.reset();
 
       this.budgetService.numPages.set(1);
       this.budgetService.numLanguages.set(1);
       } catch (error) {
-        // 4. Si el 'try' falla (per exemple, no hi ha internet o error de base de dades)
         console.error('Error en l enviament', error);
         alert('Vaja! No s ha pogut guardar el pressupost');
         }
       } else {
         console.warn('Formulari invàlid. Revisa els camps.');
-        this.budgetForm.markAllAsTouched(); // Perquè l'usuari vegi els errors en vermell
+        this.budgetForm.markAllAsTouched();
       }
 
     }
